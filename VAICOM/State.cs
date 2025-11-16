05f600a91aa27a1ea5dab4002c9d2ca81bce4961
Penecruz <73371761+Penecruz@users.noreply.github.com>
2025-11-16 15:01:57 +1100

    Release 3.0.7.6
    - Update release version to latest Beta.

--- VAICOM/State.cs
+++ VAICOM/State.cs
@@ -29,11 +29,11 @@ namespace VAICOM
         // general
 
 
-        public static bool versionbeta = true; //set if Beta version
+        public static bool versionbeta = false; //set if Beta version
         public static bool versiondev = false; //set if Dev version
         public static bool usenewselectmethod = false;
         public static string debuguser = "VAICOM_Tester";
-        public static string clientmode = ClientModes.Debug; //set to Normal for release, Debug for development
+        public static string clientmode = ClientModes.Normal; //set to Normal for release, Debug for development
 
         public static string versionstring = "";
         public static string pluginversionnumber = "3.0.6"; // used by Theme (Special page)
@@ -45,7 +45,7 @@ namespace VAICOM
         public static bool UseVoiceAccessPriority { get; set; } = false; // Default to unchecked
 
         public static bool deepdebugmode = false; //set to deepdebug mode
-        public static bool databaseencrypted = false; //set to true if database is encrypted, false if not
+        public static bool databaseencrypted = true; //set to true if database is encrypted, false if not
         public static bool luahardreset = true;
         public static bool exitapp = false;
         public static bool datawasreset = false;
